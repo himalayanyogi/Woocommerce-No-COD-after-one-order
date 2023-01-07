80d1902c00cae6eaa6598e4729a11267cf66bd7a
himalayan_yogi <sumit65284937@gmail.com>
2023-01-07 15:52:46 +0530

    Do nota llow users to use COD after onc order

---
+++ Woocommerce-No-COD-after-one-order.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name: NO COD
+Plugin URI: thehimalayanyogi.buzz
+Description: Displays the current time in India in the top menu of the WordPress dashboard.
+Version: 1.0
+Author: Shivendra Kumar
+*/
+
+function check_if_user_eligible_for_cod( $available_gateways ) {
+    if ( ! is_checkout() ) {
+        return $available_gateways;
+    }
+    $user = wp_get_current_user();
+    if ( ! $user->exists() ) {
+        return $available_gateways;
+    }
+    $orders = wc_get_orders( array(
+        'customer_id' => $user->ID,
+    ) );
+    if ( count( $orders ) > 0 && isset( $available_gateways['cod'] ) ) {
+        unset( $available_gateways['cod'] );
+        wc_add_notice( 'You are not eligible to use COD for your next order.', 'error' );
+    }
+    return $available_gateways;
+}
+add_filter( 'woocommerce_available_payment_gateways', 'check_if_user_eligible_for_cod' );
+
+
+
+
+?>
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Plugin to Allow COD for First Order Only
+Version: 1.0
+This plugin allows users to use the COD (cash on delivery) payment method for their first order, but not for subsequent orders.
+
+## Requrement:
+1. WordPress
+2. WooCommerce plugin
+
+## Installation
+1. Upload the plugin files to the /wp-content/plugins/cod-first-order-only directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+
+## Usage
+When a user places their first order, they will be able to select the COD payment method on the checkout page.
+If the user has placed an order before, the COD payment method will not be available on the checkout page, and an error message will be displayed above the "Place order" button indicating that they are not eligible to use COD for their next order.
+
+## Credit
+Author: Shivendra kr.
+
